318a0f846cdd969c0b7b0ed9501ee5dd1f7bed8c
Franco Cedillo <franco.cedillo@gmail.com>
2019-06-13 15:16:38 -0500

    creating Genesis Block

--- app.py
+++ app.py
@@ -164,3 +164,30 @@ transactions.append(t10)
 for transaction in transactions:
     display_transaction(transaction)
     print('--------------')
+
+
+class Block:
+    def __init__(self):
+        self.verified_transactions = []
+        self.previous_block_hash = ""
+        self.Nonce = ""
+
+
+last_block_hash = ""
+
+
+t0 = Transaction(
+   "Genesis",
+   Dinesh.identity,
+   500.0
+)
+
+block0 = Block()
+
+block0.previous_block_hash = None
+Nonce = None
+
+block0.verified_transactions.append(t0)
+
+digest = hash(block0)
+last_block_hash = digest
